619f364e6c6fb05c563738558dca9eff4a4c7bc5
Stipe Roguljić <stipe.roguljic@gmail.com>
2022-03-08 19:42:03 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+secret = 7
+
+guess = int(input("Pogodi tajni broj (između 1 i 10): "))
+
+if guess == secret:
+    print("Pogodio si, čestitamo! To je broj 7.")
+
+else:
+    print("Pogrešno, više sreće drugi put!")
